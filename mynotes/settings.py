28b59c19a8ff409f12867a2cd9276c847d22a4bb
Oden Baker <88295456+1LooperTrooper@users.noreply.github.com>
2022-03-31 08:42:59 +1000

    Update settings.py

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6t+6_2%!^p#y-nd2qajnqn)8-1asg*8!cp(lt35()1h&21s@1'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
